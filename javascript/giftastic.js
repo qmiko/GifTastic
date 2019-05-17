c7b16ebc0f64625f5b01061a294117a4ee747ca2
shaquima <senoritabroccolini@gmail.com>
2019-05-16 20:27:17 -0500

    css changes/updating html

---
+++ javascript/giftastic.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    var searchTerms = ['cat', 'cow', 'doggo', 'horse']
+    console.log("testing...");
+
+    var createButton = function(name){
+        var button = $('<button>');
+        button.text(name);
+        $('.button-container').append(button);
+    }
+
+    var createButtonsFromArr
+
+})
