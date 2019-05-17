1cf6faca51f8ad85e9ab92dd3610d999c9014e48
shaquima <senoritabroccolini@gmail.com>
2019-05-16 21:25:06 -0500

    js and html done

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -2,15 +2,25 @@ $(document).ready(function(){
     var searchTerms = ['cat', 'cow', 'doggo', 'horse']
     console.log("testing...");
 
+
+    $('#add-button').unbind().click(function(e){
+        e.preventDefault();
+        createButton($('#search').val().trim());
+      });
+
+
     var createButton = function(name){
+        console.log("creating Button", name);
         var button = $('<button>');
         button.text(name);
 
         button.click(function(){
             console.log($(this).text());
+            gifQuery($(this).text());
+
         })
 
-        $('.button-container').append(button);
+        $('#button-container').append(button);
 
     }
 
@@ -24,13 +34,18 @@ $(document).ready(function(){
         var apiKey ="6fqQZoEMgJpAr0tn1oUZy6BbOW5Oahc9";
         var queryURL = "http://api.giphy.com/v1/gifs/search?apiKey="
             +apiKey
-            +"&Q="+term;
+            +"&q="+term;
 
         $.ajax({
             method:"GET",
             url: queryURL,
         }).then(function(result){
             console.log(result);
+            var url = result.data[Math.floor(Math.random()*23)].images.fixed_width.url;
+            var img = $('<img>');
+            img.attr("src", url);
+            $('#gifs').append(img);
+            
             //do something cool
         })
     }
